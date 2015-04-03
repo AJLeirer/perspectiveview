35accc9a94541e9540ccfb2c13c5d5aaf6d934c6
Danny Gruebl <info@bluejade.de>
2015-04-03 17:59:47 +0200

    Method getRenderOrder() added.

--- dev/modules/render_flat.js
+++ dev/modules/render_flat.js
@@ -33,7 +33,7 @@
      *
      * @public
      * @ignore
-     * @memberof! PerspectiveView.validate
+     * @memberof! PerspectiveView.render_flat
      * @type {Object}
      */
     pub = {};
@@ -42,6 +42,43 @@
 
     // ----------------------------------------------------------------------------------------------- Private
 
+
+    /**
+     * Stores the order of to rendered map items.
+     *
+     * @private
+     * @memberof! PerspectiveView.render_flat
+     * @function
+     * @alias getRenderOrder
+     * @type {Array}
+     */
+    priv.renderOrder = [];
+
+
+
+    /**
+     * Returns an array of the ordered map items.
+     *
+     * @private
+     * @memberof! PerspectiveView.render_flat
+     * @function
+     * @alias getRenderOrder
+     * @return {Array}
+     */
+    priv.getRenderOrder = function getRenderOrder(map) {
+        var y         = map.length,
+            x         = map[0].length,
+            orderlist = [];
+
+        // The map will be rendered in reversed order, so the orderlist has also to be determent in reversed order
+        while (y--) {
+            while (x--) {
+                orderlist.push(map[y][x]);
+            }
+        }
+
+        return orderlist;
+    };
 
 
     // ------------------------------------------------------------------------------------------------ Public
